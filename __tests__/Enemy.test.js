3ec07dc4bd25253792937411c61bb5f8998aeb80
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-02-21 21:11:43 -0600

    9.3 done

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,66 @@
+
+// import and mock the needed modules
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+jest.mock('../lib/Potion.js');
+
+// starter enemy object
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
+
+// we can reuse the health and attack related tests and change player to enemy
+test("gets enemy's health value", () => {
+    // object for testing with required properties initialized
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999);
+
+    expect(enemy.health).toBe(0);
+});
+
+
+// verify the enemy has a name and weapon defined
+//  this checks to make sure that the string contains both the name and weapon, rather than checking for the exact description.
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
